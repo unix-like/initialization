7586aa9d1565043ddce2032cd7008c05799233e2
unix-like <root@unix-like.com>
2017-12-10 18:38:09 +0800

    add some

--- install_package.sh
+++ install_package.sh
@@ -7,10 +7,19 @@ apt-get -y upgrade
 apt-get install -y vim wget curl git zsh
 
 # 安装 oh my zsh
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 
 # 安装docker
+apt-get  install -y apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu xenial stable"
+apt-get update
+apt-get install -y docker-ce
+
 
 # 安装 字体选择
+
+apt-get  install -y language-selector*
 
 
